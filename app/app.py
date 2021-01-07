6d9b50b68eb7f32163af0f11530a3822583fd96a
Arib <aribc@mit.edu>
2021-01-07 13:22:05 -0500

    Some refactoring, added encrypt_id(), decrypt_id() and support for multi word titles however requires fixing on explore.html

--- app/app.py
+++ app/app.py
@@ -5,8 +5,8 @@ import sqlite3, random, os
 
 app = Flask(__name__)    #create Flask object
 
-db = sqlite3.connect("story.db") #open db if file exists, otherwise create db
-c = db.cursor()  
+db = sqlite3.connect("story.db", check_same_thread=False) #open db if file exists, otherwise create db
+c = db.cursor()
 
 app.secret_key= os.urandom(32) #Generates a random 32-byte key for the session
 
@@ -23,8 +23,6 @@ def disp_signup():
 
 @app.route("/login")
 def login():
-    db = sqlite3.connect("story.db", check_same_thread=False) #open db if file exists, otherwise create db
-    c = db.cursor()
     #Retreive the username and password from the form
     user= request.args['username']
     password= request.args['password']
@@ -37,16 +35,14 @@ def login():
         session['username']=user
         return render_template('home.html')
     #if info incorrect, show error message on login page
-    else: 
+    else:
         return render_template('login.html', err="Username or Password Incorrect")
 
 @app.route("/signup")
 def signup():
-    db = sqlite3.connect("story.db", check_same_thread=False) #open db if file exists, otherwise create db
-    c = db.cursor()
     #retreive the inputted user and password
     user= request.args['username']
-    password= request.args['password']  
+    password= request.args['password']
     try: c.execute("SELECT username FROM users WHERE username=?", (user))
     except: render_template('makeAcct.html', err="Username already exists, please try another")
     #insert the user into the table
@@ -65,16 +61,17 @@ def logout():
 def getActiveUser():
     if 'username' in session:
         return session['username']
-    return 
+    return
 
+#Encrypts and decrypts story_id so it doesn't run into errors later
+def encrypt_id(id): return id.replace(" ","10S01").replace(",","10C01") #Encrypt for DB
+def decrypt_id(id): return id.replace("10S01"," ").replace("10C01",",") #Decrypt for other purposes
 
-@app.route("/create") # , methods=['GET', 'POST'])
-def create_story() -> None: #Adding page data from created webpage to data_subtopics table in subtopics.db
-    db = sqlite3.connect("story.db", check_same_thread=False) #open db if file exists, otherwise create db
-    c = db.cursor()
-    story_id= request.args["story_id"]
-    story_contrib=request.args["story_contrib"]
-    try: c.execute("CREATE TABLE %s(id INTEGER PRIMARY KEY, user_id TEXT, story_contrib TEXT)" % story_id)
+@app.route("/create")
+def create_story() -> None:
+    story_id= request.args["story_id"] #Gets Story Title
+    story_contrib=request.args["story_contrib"] #Gets the Story Contribution
+    try: c.execute("CREATE TABLE %s(id INTEGER PRIMARY KEY, user_id TEXT, story_contrib TEXT)" % encrypt_id(story_id))
     except: return render_template("storyCreate.html", err="Title already exists, choose another title")
     db.commit()
     return story_add_helper(story_id, story_contrib)
@@ -86,18 +83,14 @@ def story_add() -> None:
     return story_add_helper(story_id, story_contrib)
 
 def story_add_helper(story_id: str, story_contrib: str) -> str:
-    db = sqlite3.connect("story.db", check_same_thread=False) 
-    c = db.cursor()
     user_id = str(getActiveUser())
-    #print(user_id)
+    story_id = encrypt_id(story_id)
     c.execute("INSERT INTO %s(user_id, story_contrib) VALUES(%s, %s);" % (story_id, '"'+user_id+'"', '"'+story_contrib+'"')) #Insert the values into the table
-    db.commit()
     c.execute("UPDATE users SET story_ids = story_ids || %s WHERE username = %s;" % ('"'+story_id+',"', '"'+user_id+'"'))
     db.commit()
     c.execute("SELECT * from " + story_id+";")
     story_bits = [row[2] for row in c.fetchall()]
-    #print("story_bits:           ", story_bits)
-    return render_template("storyView.html", story=story_bits, title=story_id) #Taking function input, not html form input
+    return render_template("storyView.html", story=story_bits, title=decrypt_id(story_id)) #Taking function input, not html form input
 
 @app.route("/renderCreate")
 def renderCreateHTML():
@@ -105,24 +98,18 @@ def renderCreateHTML():
 
 @app.route("/renderAdd")
 def renderStoryAdd():
-    db = sqlite3.connect("story.db", check_same_thread=False) 
-    c = db.cursor()
     story_id=request.args['story_id']
-    c.execute("SELECT * FROM %s ORDER BY id DESC LIMIT 1;" % story_id)
-    story_contrib = c.fetchone()[2] 
+    c.execute("SELECT * FROM %s ORDER BY id DESC LIMIT 1;" % encrypt_id(story_id))
+    story_contrib = c.fetchone()[2]
     return render_template("storyAdd.html", title= story_id, old_story_contrib = story_contrib)
 
-@app.route("/renderView")  
+@app.route("/renderView")
 def renderFullStory():
     return render_template("storyView.html", story=renderStory(request.args['story_id']))
 
 def renderStory(story_id):
-    db = sqlite3.connect("story.db", check_same_thread=False) 
-    c = db.cursor()
-    try: c.execute("SELECT * from " + story_id+";")
-    except: print("no!")
+    c.execute("SELECT * from " + encrypt_id(story_id)+";")
     story_bits = [row[2] for row in c.fetchall()]
-    print("story_bits:           ", story_bits)
     return render_template("storyView.html")
 
 @app.route("/home")
@@ -136,12 +123,12 @@ def render_home():
     stories=list(set(item[0] for item in c.fetchall()) - {"users"})
     db.commit()
     db.close()
-    db = sqlite3.connect("story.db", check_same_thread=False) 
+    db = sqlite3.connect("story.db", check_same_thread=False)
     c = db.cursor()
     print(stories)
     story_titles=[]
     for title in stories:
-        try: 
+        try:
             print("SELECT user_id FROM "+title+ " WHERE user_id='"+username+"'")
             c.execute("SELECT user_id FROM "+title+ " WHERE user_id='"+username+"'")
             #c.execute("SELECT EXISTS(SELECT 1 FROM ? WHERE user_id=?);", (title, username))
@@ -158,43 +145,37 @@ def render_home():
     # user = c.fetchone()                #user stores first line of user table
     # while username!= user[0]:      #while form username != table username
     #     user = c.fetchone()                         #move on to the next line of user table
-    
+
     # story_ids_str = user[2]                 #user's story_ids as a string (i.e. directly from users table)
     # story_ids = story_ids_str.split(',')    #list of user's story_ids
-    
+
     # my_stories = []             #list of stories user has entries in
-    
+
     # for story_id in story_ids:
     #     c.execute("SELECT * FROM %s ORDER BY id ASC;" %story_id)
-        
+
     #     story = ""
     #     for entry in c.fetchall():
     #         story += entry[2]
-            
+
     #     my_stories.append(story)
-    
+
     # render_template("home.html", story_titles = my_stories, title=story_id)
 
 #shows list of story titles on explore page
 @app.route("/explore")
 def explore():
-    db = sqlite3.connect("story.db", check_same_thread=False) 
-    c = db.cursor()
     username= getActiveUser()
-    print(username)
     c.execute('SELECT name from sqlite_master where type= "table"')
-    print(c.fetchall)
-    stories=list(set(item[0] for item in c.fetchall()) - {"users"})
-    story_titles=[]
-    #DOESNT WORK YET
-    for title in stories:
-        try: 
-            c.execute("SELECT user_id FROM "+title+ " WHERE user_id!='"+username+"'")
-            story_titles+=[title]
-        except: print("oops")
-    print(story_titles)
-    return render_template("explore.html", story_titles=stories)
+    all_stories=set(decrypt_id(item[0]) for item in c.fetchall()) - {"users"}
+    c.execute('SELECT * FROM users;')
+    user_stories = set()
+    for row in c.fetchall():
+        if row[0] == username:
+            user_stories = set(row[2].split(","))
+    story_titles = list(all_stories - user_stories)
+    return render_template("explore.html", story_titles=story_titles)
 
-if __name__ == "__main__": 
+if __name__ == "__main__":
     app.debug = True
     app.run()
